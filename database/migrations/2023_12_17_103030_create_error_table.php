60d59505c3291e863896a3e6c6aee7b1d48568d4
Peter Somlay <p.somlay@gmail.com>
2023-12-17 11:36:57 +0100

    Error datatable create

---
+++ database/migrations/2023_12_17_103030_create_error_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateErrorTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('error', function (Blueprint $table) {
+            $table->id('error_id');
+            $table->date('error_date');
+            $table->text('error_text');
+            $table->string('error_who');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('error');
+    }
+}
