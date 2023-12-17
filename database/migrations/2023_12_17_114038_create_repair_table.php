e66d181d39c2b2678dcc7e35e5c53bc2793fd861
Peter Somlay <p.somlay@gmail.com>
2023-12-17 12:48:01 +0100

    Repair datatable create

---
+++ database/migrations/2023_12_17_114038_create_repair_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRepairTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('repair', function (Blueprint $table) {
+            $table->id('repair_id');
+            $table->string('worksheet_number');
+            $table->string('user_name');
+            $table->string('user_mobile');
+            $table->string('user_email');
+            $table->string('repair_parts');
+            $table->unsignedBigInteger('mechanic_id');
+            $table->string('work_status');
+            $table->string('call_status');
+            $table->date('finish_date');
+            $table->biginteger('all_parts');
+            $table->biginteger('all_work');
+            $table->biginteger('all_price');
+            $table->longtext('repair_document');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('repair');
+    }
+}
