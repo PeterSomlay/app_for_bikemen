51d98ff19ee3542ee8d2777ec31061849588c6d1
Peter Somlay <p.somlay@gmail.com>
2023-12-17 11:43:04 +0100

    Documents datatable create

---
+++ database/migrations/2023_12_17_103816_create_documents_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDocumentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('documents', function (Blueprint $table) {
+            $table->id('doc_id');
+            $table->date('doc_date');
+            $table->string('doc_worksheet_number');
+            $table->string('doc_username');
+            $table->string('doc_parts');
+            $table->longtext('doc_document');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('documents');
+    }
+}
