227d391d6e4a6ea81ccd60f24f493114ebd1350e
Peter Somlay <p.somlay@gmail.com>
2023-12-17 10:34:34 +0100

    Tools datatable create

---
+++ database/migrations/2023_12_17_093021_create_tools_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateToolsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tools', function (Blueprint $table) {
+            $table->id('tools_id');
+            $table->date('tools_date');
+            $table->string('tools_name');
+            $table->string('tools_where');
+            $table->string('tools_who');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tools');
+    }
+}
