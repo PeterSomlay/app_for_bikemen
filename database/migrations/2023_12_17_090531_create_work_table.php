98f389109a9398f1094739ad9a59897ce5852fd6
Peter Somlay <p.somlay@gmail.com>
2023-12-17 10:14:32 +0100

    Work datatable create

---
+++ database/migrations/2023_12_17_090531_create_work_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWorkTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('work', function (Blueprint $table) {
+            $table->id('work_id');
+            $table->string('work_name');
+            $table->biginteger('work_price');
+            $table->string('worksheet_number');
+            $table->integer('status_w');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('work');
+    }
+}
