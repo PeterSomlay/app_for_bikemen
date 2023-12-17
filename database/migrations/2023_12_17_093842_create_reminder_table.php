3ff1b495ae661f94b16345a0640ad624264b8337
Peter Somlay <p.somlay@gmail.com>
2023-12-17 10:41:31 +0100

    Reminder datatable create

---
+++ database/migrations/2023_12_17_093842_create_reminder_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReminderTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reminder', function (Blueprint $table) {
+            $table->id('reminder_id');
+            $table->date('reminder_date');
+            $table->text('reminder_comment');
+            $table->string('reminder_admin');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reminder');
+    }
+}
