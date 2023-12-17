2375221beb6be28bb82d5afaec95c04a8d93f5ae
Peter Somlay <p.somlay@gmail.com>
2023-12-17 11:29:54 +0100

    Events datatable create

---
+++ database/migrations/2023_12_17_102700_create_events_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id('events_id');
+            $table->string('events_name');
+            $table->string('events_year');
+            $table->string('events_mounth');
+            $table->string('events_day');
+            $table->string('events_status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
